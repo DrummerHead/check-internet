1ab781be87d824612fe14675b168a632dabb9b6c
DrummerHead <drummerhead@gmail.com>
2013-05-28 01:20:41 -0300

    Add counter, tidy up code

--- js/gedrag.js
+++ js/gedrag.js
@@ -1,10 +1,16 @@
 $(function(){
-  var cosomn = "<iframe id='hohoooo' width='800' height='600' src='http://duckduckgo.com/'></iframe>"
-  var $vodi = $('body')
+  var cosomn = "<iframe id='hohoooo' width='800' height='600' frameborder='0' src='http://duckduckgo.com/'></iframe>";
+  var $cante = $('h2 span');
+  var cante = 0;
+
   var metele = function(){
     $('#hohoooo').remove();
     $('body').append(cosomn);
+
+    cante++
+    $cante.text(cante)
   }
+
   metele();
   var amego = setInterval(metele, 60000);
 })

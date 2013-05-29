08fbb8600785f664cc77bac4be4c8adee839469f
DrummerHead <drummerhead@gmail.com>
2013-05-28 21:24:21 -0300

    Add success and failure counter

--- js/gedrag.js
+++ js/gedrag.js
@@ -1,18 +1,27 @@
 $(function(){
   var cosomn = "<iframe id='hohoooo' width='800' height='600' frameborder='0' src='http://duckduckgo.com/'></iframe>";
-  var $cante = $('h2 span');
-  var cante = 0;
+  var $cante = $('#cante');
+  var $happy = $('#happy');
+  var $sad = $('#sad');
+  var cante = -1;
+  window.happy = 0;
+  window.sad = -1;
 
   var metele = function(){
     $('#hohoooo').remove();
     $('body').append(cosomn);
 
-    cante++
-    $cante.text(cante)
+    window.sad++;
+    cante++;
+    $('#moribus').append("<script src='http://mcdlr.com/sup-brah/lupites.js'></script>");
+
+    $cante.text(cante);
+    $happy.text(window.happy)
+    $sad.text(window.sad)
   }
 
   metele();
-  var amego = setInterval(metele, 60000);
+  var amego = setInterval(metele, 60*1000);
 })
 
 
---
+++ js/lupites.js
@@ -0,0 +1,2 @@
+window.sad--;
+window.happy++;
